089e3faf74713bd37b75063fab5a97a233179d0a
GoodManWEN <44322449+GoodManWEN@users.noreply.github.com>
2022-04-27 03:13:00 +0100

    Update ubuntu_init.sh

--- ubuntu_init.sh
+++ ubuntu_init.sh
@@ -118,3 +118,4 @@ rm tmp.py
 curl -sSO https://raw.githubusercontent.com/zhucaidan/btpanel-v7.7.0/main/install/install_panel.sh && bash install_panel.sh
 sed -i "s|if (bind_user == 'True') {|if (bind_user == 'REMOVED') {|g" /www/server/panel/BTPanel/static/js/index.js
 rm -rf /www/server/panel/data/bind.pl
+rm -f /www/server/panel/data/bind.pl
