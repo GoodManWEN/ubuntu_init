810ee01b8f1a0d48b56c8b7e040ede277f8c690a
GoodManWEN <44322449+GoodManWEN@users.noreply.github.com>
2022-05-23 16:40:22 +0100

    Create oracle_linux_auto_install_configure.sh

---
+++ misc/oracle_linux_auto_install_configure.sh
@@ -0,0 +1,138 @@
+yum install -y libXrender libXrender.so.1 yum-utils
+
+
+rpm -v --import https://download.sublimetext.com/sublimehq-rpm-pub.gpg
+sudo yum-config-manager --add-repo https://download.sublimetext.com/rpm/stable/x86_64/sublime-text.repo
+
+yum install -y sublime-text
+
+dnf install -y oracle-database-preinstall-19c
+
+/sbin/sysctl -p
+
+dnf install -y bc    
+dnf install -y binutils
+#dnf install -y compat-libcap1
+dnf install -y compat-libstdc++-33
+#dnf install -y dtrace-modules
+#dnf install -y dtrace-modules-headers
+#dnf install -y dtrace-modules-provider-headers
+#dnf install -y dtrace-utils
+dnf install -y elfutils-libelf
+dnf install -y elfutils-libelf-devel
+dnf install -y fontconfig-devel
+dnf install -y glibc
+dnf install -y glibc-devel
+dnf install -y ksh
+dnf install -y libaio
+dnf install -y libaio-devel
+#dnf install -y libdtrace-ctf-devel
+dnf install -y libXrender
+dnf install -y libXrender-devel
+dnf install -y libX11
+dnf install -y libXau
+dnf install -y libXi
+dnf install -y libXtst
+dnf install -y libgcc
+dnf install -y librdmacm-devel
+dnf install -y libstdc++
+dnf install -y libstdc++-devel
+dnf install -y libxcb
+dnf install -y make
+dnf install -y net-tools # Clusterware
+dnf install -y nfs-utils # ACFS
+dnf install -y python # ACFS
+dnf install -y python-configshell # ACFS
+dnf install -y python-rtslib # ACFS
+dnf install -y python-six # ACFS
+dnf install -y targetcli # ACFS
+dnf install -y smartmontools
+dnf install -y sysstat
+
+# Added by me.
+dnf install -y unixODBC
+
+# New for OL8
+dnf install -y libnsl
+dnf install -y libnsl.i686
+dnf install -y libnsl2
+dnf install -y libnsl2.i686
+
+groupadd -g 54321 oinstall
+groupadd -g 54322 dba
+groupadd -g 54323 oper 
+#groupadd -g 54324 backupdba
+#groupadd -g 54325 dgdba
+#groupadd -g 54326 kmdba
+#groupadd -g 54327 asmdba
+#groupadd -g 54328 asmoper
+#groupadd -g 54329 asmadmin
+#groupadd -g 54330 racdba
+
+useradd -u 54321 -g oinstall -G dba,oper oracle
+
+sed -i "s|SELINUX=enforcing|SELINUX=permissive|g" /etc/selinux/config
+
+setenforce Permissive
+
+systemctl stop firewalld
+systemctl disable firewalld
+
+mkdir -p /u01/app/oracle/product/19.0.0/dbhome_1
+mkdir -p /u02/oradata
+chown -R oracle:oinstall /u01 /u02
+chmod -R 775 /u01 /u02
+
+mkdir /home/oracle/scripts
+
+cat > /home/oracle/scripts/setEnv.sh <<EOF
+# Oracle Settings
+export TMP=/tmp
+export TMPDIR=\$TMP
+
+export ORACLE_HOSTNAME=ol8-19.localdomain
+export ORACLE_UNQNAME=cdb1
+export ORACLE_BASE=/u01/app/oracle
+export ORACLE_HOME=\$ORACLE_BASE/product/19.0.0/dbhome_1
+export ORA_INVENTORY=/u01/app/oraInventory
+export ORACLE_SID=cdb1
+export PDB_NAME=pdb1
+export DATA_DIR=/u02/oradata
+
+export PATH=/usr/sbin:/usr/local/bin:\$PATH
+export PATH=\$ORACLE_HOME/bin:\$PATH
+
+export LD_LIBRARY_PATH=\$ORACLE_HOME/lib:/lib:/usr/lib
+export CLASSPATH=\$ORACLE_HOME/jlib:\$ORACLE_HOME/rdbms/jlib
+EOF
+
+echo ". /home/oracle/scripts/setEnv.sh" >> /home/oracle/.bash_profile
+
+
+cat > /home/oracle/scripts/start_all.sh <<EOF
+#!/bin/bash
+. /home/oracle/scripts/setEnv.sh
+
+export ORAENV_ASK=NO
+. oraenv
+export ORAENV_ASK=YES
+
+dbstart \$ORACLE_HOME
+EOF
+
+
+cat > /home/oracle/scripts/stop_all.sh <<EOF
+#!/bin/bash
+. /home/oracle/scripts/setEnv.sh
+
+export ORAENV_ASK=NO
+. oraenv
+export ORAENV_ASK=YES
+
+dbshut \$ORACLE_HOME
+EOF
+
+chown -R oracle:oinstall /home/oracle/scripts
+chmod u+x /home/oracle/scripts/*.sh
+
+reboot
